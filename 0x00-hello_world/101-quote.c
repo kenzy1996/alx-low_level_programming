0c8a28fdfd2df1b5993ef6b6fe9d1369f441fcf3
kenzy1996 <swagkenz11@gmail.com>
2023-02-16 08:39:48 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A c program that print a line to the standard error
  * Return 1 (success)
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
