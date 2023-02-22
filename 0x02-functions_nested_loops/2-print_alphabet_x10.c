bb4eb6069856c331c5687ebf123e1f77f9ac4742
kenzy1996 <swagkenz11@gmail.com>
2023-02-22 14:18:55 -0800

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,21 @@
 
 /**
  * print_alphabet_x10 - print 10 times the alphabet in lower cases
+ *
+ * Return: 0
+ *
 */
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int ten;
+	int c;
 
-	while (count++ <= 9)
+	for (ten = 0; ten <= 9; ten++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-	}
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 	_putchar('\n');
+	}
 }
