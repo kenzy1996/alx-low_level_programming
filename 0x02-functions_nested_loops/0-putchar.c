2985f6a5b0ddeb194b937eb859e7acc9c8633bcf
kenzy1996 <swagkenz11@gmail.com>
2023-02-21 18:33:45 -0800

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,12 @@
 /**
  * _putchar - writes the character to stdout
  *@c: The character to print
-
+ *
  * Return: 1
  * On error, -1 is returned, and errno is set appropriately.
  */
 
 int _putchar(char c)
 {
-	return (write (1, &c, 1));
+	return (write(1, &c, 1));
 }
