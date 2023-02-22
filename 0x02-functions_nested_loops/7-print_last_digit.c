0b7a53f7c31be204952d10ffc3397cd7f5eddd9c
kenzy1996 <swagkenz11@gmail.com>
2023-02-22 15:57:45 -0800

    comit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main"
+/**
+ * print_last_digit - prints last digit
+ * @n: the number
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int ld;
+
+	if (n < 0)
+	{
+		ld = (-1 * (n % 10));
+		_putchar(ld + '0');
+		return (ld);
+	}
+	else
+	{
+		ld = (n % 10);
+		_putchar(ld + '0');
+		return (ld);
+	}
+}
