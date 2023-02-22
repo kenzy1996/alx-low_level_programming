7be8fdc2e972104f054173b9d99a0f4a03a655f0
kenzy1996 <swagkenz11@gmail.com>
2023-02-21 19:18:30 -0800

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
-/**
- * print_alphabet - print all alphabets in lower case
- */
 
-void print_alphabets(void)
+/**
+ * print_alphabet - print all alphabet in lower case
+*/
+
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar('\n');
 }
