fadc5d76073ea80ea1499fc0b1527a77f0b1f53b
kenzy1996 <swagkenz11@gmail.com>
2023-02-23 03:13:34 -0800

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,10 @@
  * @c: input
  * Return: 1 if c is uppercase, 0 if otherwise
  */
-
 int _isupper(int c)
 {
-	if ((c >= 'A') && (C <= 'Z'))
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
 	else
 		return (0);
-}	
+}
