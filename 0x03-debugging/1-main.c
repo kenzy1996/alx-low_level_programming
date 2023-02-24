8bde0d1f9792ab56421e5f43ac98852645b06de4
kenzy1996 <swagkenz11@gmail.com>
2023-02-23 16:03:47 -0800

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -8,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 	/**
@@ -17,8 +18,7 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-
-	printf("infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
