58f2db2474be7a7ff66edad754689aeb34edb921
kenzy1996 <swagkenz11@gmail.com>
2023-02-27 14:24:04 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function that copies the string pointed to by src
+ * @dest: assigned
+ * @src: assigned
+ *
+ * Return: the return copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src;
+	}
+	*dest = '\0';
+	return (start);
+}
