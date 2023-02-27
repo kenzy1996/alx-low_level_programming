f2cb6ddc9f1c4941c77a1f24aafe7359e0126ccf
kenzy1996 <swagkenz11@gmail.com>
2023-02-27 11:15:55 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev - a function that prints a string, in reverse
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0', index++)
+		;
+	for (index = index - 1; s[index] != '\0', index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
