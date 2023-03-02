12edbf4acf5041ed1566c175d633d00ee3b4efe9
kenzy1996 <swagkenz11@gmail.com>
2023-03-02 10:46:04 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: the first number to be added
+ * @n2: the second number to be added
+ * @r: the buffer to store the result
+ * size_r: the buffer size
+ *
+ * Return: if r can store the sum - a pointer to the result
+ *         if r cannot store the sum - 0
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
+
+/**
+ * add_strings - adds the number stored in two strings
+ * @n1: The string containing the first number to be added
+ * @n2: the string containing the second number to be added
+ * @r: the buffer to store the result
+ * @r_index: the current index of the buffer
+ *
+ * Return: if r can store the sum - a pointer to the result
+ *         if r cannot store the sum - 0
+ */
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (num % 10) + '0';
+	return (r + r_index);
+	}
+	else if (tens && r_index < 0)
+		return (0);
+
+	return (r + r_index + 1);
+}
